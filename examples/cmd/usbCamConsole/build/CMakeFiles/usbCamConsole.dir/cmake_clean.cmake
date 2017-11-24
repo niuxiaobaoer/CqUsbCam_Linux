27c678b92004f1570864cf3c8a1cdc6d7ba0ad0e
nxb <nxb@ubuntu14.04>
2017-11-25 00:37:04 +0800

    add cmd examples

---
+++ examples/cmd/usbCamConsole/build/CMakeFiles/usbCamConsole.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/usbCamConsole.dir/main.cpp.o"
+  "usbCamConsole.pdb"
+  "usbCamConsole"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/usbCamConsole.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
