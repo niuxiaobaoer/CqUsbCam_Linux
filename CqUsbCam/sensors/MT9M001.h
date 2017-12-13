3ab4c162944684a813d42577e139ebcd57f4ff5f
nxb <nxb@ubuntu14.04>
2017-12-13 21:18:48 +0800

    add support for MT9M001

---
+++ CqUsbCam/sensors/MT9M001.h
@@ -0,0 +1,30 @@
+#ifndef _MY9M001_H_
+#define _MY9M001_H_
+
+/****************************************************************************************\
+ * This is the header file for the Caiqi usb camera for Linux/Mac						*
+ *                                                                                	*
+ * Author              :        nxb     											* 												
+ * License             :        GPL Ver 2.0                                       	*
+ * Copyright           :        Caiqi Electronics Inc.								*
+ * Date written        :        nov 05, 2017                                    	*
+ * Modification Notes  :                                                          	*
+ *    1. nxb, nov 05, 2017                                  						*
+ *       Add documentation.                                          				*
+ *                      															*
+ *                                                                                	*
+ \***************************************************************************************/
+
+#include <stdio.h>
+#include <string>
+#include <list>
+
+#include "../Types.h"
+#include "../cyusb.h"
+#include "../tagSensor.h"
+
+
+void RegisterSensor_MT9M001(list<tagSensor>& sensorList);
+
+
+#endif
